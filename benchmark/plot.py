84a9623a022cbaedb26f90343b24e9540996c369
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-03-22 10:25:34 +0000

    feat(benchmark/plot.py): add grouped bar chart for all features

--- benchmark/plot.py
+++ benchmark/plot.py
@@ -1,31 +1,57 @@
 import matplotlib.pyplot as plt
-import os
+import numpy as np
 import pandas as pd
+import os
 
 base_dir = os.path.dirname(os.path.realpath(__file__))
 
 data = pd.read_csv(f'{base_dir}/binTree-benchmark.csv', delimiter=';')
 
-header_info: list[str] = list(data.columns)
-first_column = data.iloc[:, 0]
+features: list[str] = list(data.columns)[2:]
 
-for column in data.columns[1:]:
-    plt.figure()
-    bar_plot = plt.bar(first_column, data[column])
-    plt.xlabel(header_info[0])
-    plt.ylabel(column)
-    title = f'Graph of {header_info[0]} vs {column}'
-    plt.title(title)
+GROUP1: str = 'Structure'
+GROUP2: str = 'Group'
 
-    for bar in bar_plot:
-        plt.text(
-                bar.get_x() + bar.get_width() / 2,
-                bar.get_height(),
-                round(bar.get_height(), 2),
-                ha='center',
-                va='bottom'
-                )
+structures = data[GROUP1].unique()
+groups = data[GROUP2].unique()
 
-    plt.savefig(f'{base_dir}/{title}.png')
+means = {}
 
-plt.show()
+for feature in features:
+    means[feature] = {}
+    for group in groups:
+        means[feature][group] = data[data[GROUP2] == group][feature].values
+
+print(means)
+
+def plot_grouped_bar(group1: str, group2: str, feature: str, *, image_filepath: str = ''):
+    if any([len(arg) == 0 for arg in (group1, group2, feature)]):
+        return
+
+    _, ax = plt.subplots()
+
+    X = np.arange(len(structures))
+    WIDTH = 0.25
+    MULTIPLIER = 0
+
+    for group, measurement in means[feature].items():
+        offset = WIDTH * MULTIPLIER
+        rects = ax.bar(X + offset, measurement, WIDTH, label=group)
+        ax.bar_label(rects, padding=3)
+        MULTIPLIER += 1
+
+    ax.set_ylabel(feature)
+    ax.set_title(f'{feature} by {group1} and {group2}')
+    ax.set_xticks(X + WIDTH, structures)
+    ax.legend(loc='upper left', bbox_to_anchor=(1, 1), title=GROUP2)
+
+    if len(image_filepath) != 0:
+        plt.savefig(image_filepath)
+
+for feature in ('Insertion Time of 10000 keys (seconds)', 'Rotations'):
+    plot_grouped_bar(
+            GROUP1,
+            GROUP2,
+            feature,
+            image_filepath=f'{base_dir}/{feature}.png'
+            )
